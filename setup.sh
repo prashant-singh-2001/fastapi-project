2f283d94fe3a13168406437dbf62a03a729ab997
Prashant Singh <ps47600@gmail.com>
2026-05-17 22:06:18 +0530

    to run the program, run setup file

---
+++ setup.sh
@@ -0,0 +1,18 @@
+@echo off
+
+echo Creating virtual environment...
+python -m venv venv
+
+echo Activating virtual environment...
+call venv\Scripts\activate
+
+echo Upgrading pip...
+python -m pip install --upgrade pip
+
+echo Installing requirements...
+pip install -r requirements.txt
+
+echo.
+echo Setup completed successfully!
+echo To activate the environment later use:
+echo venv\Scripts\activate
