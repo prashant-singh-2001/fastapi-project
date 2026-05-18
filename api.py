20892ab5fd4c6f3d1cc9d5b98c5413df3f279ccf
Mohit Nagde <mohitnagde850@gmail.com>
2026-05-18 23:13:13 +0530

    Built api's using Fast-API

---
+++ api.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/myapi")
+def user():
+    return {"API": "My first API"}
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    age: int
+
+
+# POST API
+@app.post("/users")
+def create_user(user: User):
+
+    return {
+        "message": "User created successfully",
+        "data": user
+    }
+
+
+@app.get("/addition")
+def addition():
+    var = 1+2+3
+    return {"API": var}
+
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+
+    return {
+        "user_id": user_id
+    }
+
+@app.get("/users")
+def get_users(age: int):
+
+    return {
+        "age": age
+    }
+
+
+@app.get("/user/{user_id}")
+def get_user(user_id: int, active: bool):
+
+    return {
+        "user_id": user_id,
+        "active": active
+    }
